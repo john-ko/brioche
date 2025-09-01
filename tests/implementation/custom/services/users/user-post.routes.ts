469fac323459db6e9780cf103f684224a4afadca
John Ko <johnko426@gmail.com>
2025-09-01 12:43:28 -0700

    chore: remove unused tests and update tsconfig to emit types

--- tests/implementation/custom/services/users/user-post.routes.ts
+++
@@ -1,9 +0,0 @@
-export const UserGetRoute = {
-  path: '/api/users',
-  method: 'GET',
-  validation: {
-    query: () => true,
-    body: () => true,
-  },
-  handler: () => new Response('list users')
-}
